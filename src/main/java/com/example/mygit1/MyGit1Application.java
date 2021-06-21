f82d43b8860033e3933daef9104dce0d277b3e96
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-21 18:32:18 +0900

    Initial commit

---
+++ src/main/java/com/example/mygit1/MyGit1Application.java
@@ -0,0 +1,13 @@
+package com.example.mygit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGit1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mygit1/MyGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygit1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGit1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
