af71931b4c4d8f8295b65bb7796e30b58cb8b378
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-21 18:37:05 +0900

    devB branch : B생성자 만든 후 A에서 B 생성자 테스트

--- src/main/java/com/example/mygit1/A.java
+++ src/main/java/com/example/mygit1/A.java
@@ -1,4 +1,11 @@
 package com.example.mygit1;
 
 public class A {
+    private static String myName="이하연";
+    private static int myNum=2017301063;
+
+    public static void main(String[] args){
+        B print = new B();
+        System.out.println(print.introduce(myName,myNum));
+    }
 }
