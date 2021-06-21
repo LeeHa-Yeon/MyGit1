efe5732384e7bee3b25e8b75509245c1814f2424
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-21 18:44:47 +0900

    devC branch : C생성자 만든 후 A에서 C 생성자 테스트

--- src/main/java/com/example/mygit1/C.java
+++ src/main/java/com/example/mygit1/C.java
@@ -1,4 +1,26 @@
 package com.example.mygit1;
 
 public class C {
+    private String school;
+    private String department;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String introduce(String school, String department){
+        return "저의 학교는 "+school+"이고 학과는 "+department + "입니다.";
+    }
 }
